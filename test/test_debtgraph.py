763eef4446abaab32e988e6564d09c683ef837e1
James Porter <porterjamesj@gmail.com>
2013-07-06 14:06:44 -0400

    Begin adding tests for the debtgraph module.

---
+++ test/test_debtgraph.py
@@ -0,0 +1,46 @@
+from debtgraph import DebtGraph
+from nose.tools import raises
+
+class TestInit():
+    """Initialization should occur properly."""
+    def test_string_init(self):
+        """Should correctly initialize a graph from a list of names."""
+        dg = DebtGraph(["a","b","c"])
+        assert dg.graph == {"a": {},"b": {},"c": {}}
+
+    def test_dict_init(self):
+        """Should correctly initialize a graph from a dictionary."""
+        dg = DebtGraph({"a":{"b":20},"b":{}})
+        assert dg.graph == {"a":{"b":20},"b":{}}
+
+class TestRemove():
+    """Graph manipulation methods should work properly."""
+    @classmethod
+    def setUp(self):
+        """Set up a test DebtGraph."""
+        self.dg = DebtGraph(
+            {"alice": {"bob": 20, "charlie":5},
+             "bob": {"alice":10},
+             "charlie": {}})
+
+    def test_remove_normal(self):
+        """Should remove debt correctly."""
+        self.dg.remove("alice","bob",5)
+        assert self.dg.graph == {"alice": {"bob": 15, "charlie":5},
+                                 "bob": {"alice":10},
+                                 "charlie": {}}
+
+    def test_remove_deletion(self):
+        """Should delete the node if more debt is removed than exists."""
+        self.dg.remove("alice","bob",25)
+        assert self.dg.graph == {"alice": {"charlie":5},
+                                 "bob": {"alice":10},
+                                 "charlie": {}}
+
+    @raises(KeyError)
+    def test_remove_error(self):
+        """Should throw an error when trying to remove a nonexistant debt."""
+        self.dg.remove("charlie","bob",4)
+
+
+class TestForgive
